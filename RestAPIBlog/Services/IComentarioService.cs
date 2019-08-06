d8e3a18fd05d2a2c917b5d2874661914dd9c950c
Higor Hotz <highotz.hotz@hotmail.com>
2019-08-06 00:37:28 -0300

    removing unnecessary imports

--- RestAPIBlog/Services/IComentarioService.cs
+++ RestAPIBlog/Services/IComentarioService.cs
@@ -1,8 +1,5 @@
 ﻿using RestAPIBlog.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestAPIBlog.Services
 {
--- RestAPIBlog/Services/Implementations/UsuarioServiceImpl.cs
+++ RestAPIBlog/Services/Implementations/UsuarioServiceImpl.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using RestAPIBlog.Models;
 
 namespace RestAPIBlog.Services.Implementations
